b883e7425485db41954deb6e0ae1b6e87c5b9217
daga2003 <fernandadaga01@gmail.com>
2025-08-31 22:23:44 -0300

    Update Filmes.java

--- Filmes.java
+++ Filmes.java
@@ -1,3 +1,12 @@
+/**
+ * Classe que representa um Filme com suas principais características.
+ *
+ * Contém informações como título, ano de lançamento, gênero,
+ * diretor e nota média atribuída ao filme.
+ *
+ * @author Fernanda
+ * @version 1.0
+ */
 public class Filmes {
     private String titulo;
     private int anoFilme;
@@ -5,7 +14,15 @@ public class Filmes {
     private String diretor;
     private double notaMedia;
 
-    // Construtor
+    /**
+     * Construtor da classe Filmes.
+     *
+     * @param titulo    Título do filme
+     * @param anoFilme  Ano de lançamento do filme
+     * @param genero    Gênero do filme
+     * @param diretor   Diretor do filme
+     * @param notaMedia Nota média atribuída ao filme
+     */
     public Filmes(String titulo, int anoFilme, String genero, String diretor, double notaMedia) {
         this.titulo = titulo;
         this.anoFilme = anoFilme;
@@ -14,45 +31,53 @@ public class Filmes {
         this.notaMedia = notaMedia;
     }
 
-    // Getters e Setters
+    /** @return o título do filme */
     public String getTitulo() {
         return titulo;
     }
 
+    /** @param titulo Define o título do filme */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    /** @return o ano de lançamento do filme */
     public int getAnoFilme() {
         return anoFilme;
     }
 
+    /** @param anoFilme Define o ano de lançamento do filme */
     public void setAnoFilme(int anoFilme) {
         this.anoFilme = anoFilme;
     }
 
+    /** @return o gênero do filme */
     public String getGenero() {
         return genero;
     }
 
+    /** @param genero Define o gênero do filme */
     public void setGenero(String genero) {
         this.genero = genero;
     }
 
+    /** @return o diretor do filme */
     public String getDiretor() {
         return diretor;
     }
 
+    /** @param diretor Define o diretor do filme */
     public void setDiretor(String diretor) {
         this.diretor = diretor;
     }
 
+    /** @return a nota média do filme */
     public double getNotaMedia() {
         return notaMedia;
     }
 
+    /** @param notaMedia Define a nota média do filme */
     public void setNotaMedia(double notaMedia) {
         this.notaMedia = notaMedia;
     }
-
 }
